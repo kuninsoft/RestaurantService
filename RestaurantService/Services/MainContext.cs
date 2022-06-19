cf60c2dee81c817d00064a0bf9fde62275acbe11
kuninsoft <kuninsoft@gmail.com>
2022-06-19 23:00:33 +0300

    Added explicit user dbset

--- RestaurantService/Services/MainContext.cs
+++ RestaurantService/Services/MainContext.cs
@@ -10,6 +10,7 @@ public sealed class MainContext : DbContext
     public DbSet<Restaurant> Restaurants { get; set; }
     public DbSet<Dish> Dishes { get; set; }
     public DbSet<Rating> Ratings { get; set; }
+    public DbSet<User> Users { get; set; }
     
     public MainContext()
     {
